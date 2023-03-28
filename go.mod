482bc47f7bab6b9501409ede223613f4e252c7fd
shiyc <10963665+shiyuchen-tech@user.noreply.gitee.com>
2023-03-28 14:40:56 +0800

    feta: run project successfully

--- go.mod
+++ go.mod
@@ -3,6 +3,15 @@ module github.com/kalesn/gpu-memory-exporter
 go 1.16
 
 require (
-    github.com/prometheus/client_golang v1.11.0
-    github.com/prometheus/common v0.11.0
+	github.com/Microsoft/go-winio v0.6.0 // indirect
+	github.com/docker/distribution v2.8.1+incompatible // indirect
+	github.com/docker/docker v23.0.1+incompatible // indirect
+	github.com/docker/go-connections v0.4.0 // indirect
+	github.com/docker/go-units v0.5.0 // indirect
+	github.com/go-kit/kit v0.10.0 // indirect
+	github.com/opencontainers/go-digest v1.0.0 // indirect
+	github.com/opencontainers/image-spec v1.0.2 // indirect
+	github.com/prometheus/client_golang v1.14.0
+	github.com/prometheus/common v0.37.0
+
 )
--- main.go
+++ main.go
@@ -1,74 +1,138 @@
 package main
 
 import (
-    "fmt"
-    "os/exec"
-    "regexp"
-    "strconv"
-    "strings"
-    "net/http"
+	"context"
+	"errors"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"net/http"
+	"os/exec"
+	"regexp"
+	"strconv"
+	"strings"
+	"time"
 
-    "github.com/prometheus/client_golang/prometheus"
-    "github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/docker/docker/client"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 const (
-    gpuMemRegexPattern = `(\d+)MiB \/ (\d+)MiB`
-    pidRegexPattern    = `(\d+)\.h264_videorecorder`
+	//gpuMemRegexPattern = `(\d+)MiB \/ (\d+)MiB`
+	//pidRegexPattern    = `(\d+)MiB \|$`
+	pidRegexPattern = `(\d+)MiB \|$`
 )
 
 var (
-    gpuUsage = prometheus.NewGaugeVec(
-        prometheus.GaugeOpts{
-            Name: "gpu_memory_usage",
-            Help: "GPU memory usage per process",
-        },
-        []string{"pid", "docker_hostname"},
-    )
+	gpuUsage = prometheus.NewGaugeVec(
+		prometheus.GaugeOpts{
+			Name: "gpu_memory_usage",
+			Help: "GPU memory usage per process",
+		},
+		[]string{"pid", "docker_hostname"},
+	)
 )
 
+var PidSlice []int
+var containerInfos []ContainerInfo
+
 func init() {
-    prometheus.MustRegister(gpuUsage)
+	prometheus.MustRegister(gpuUsage)
+}
+
+type ContainerInfo struct {
+	ID       string
+	Pid      int
+	Hostname string
+}
+
+func InSlice(item int) bool {
+	for _, eachItem := range PidSlice {
+		if item == eachItem {
+			return true
+		}
+	}
+	return false
+}
+func GetContainerHostname(pid int) (string, error) {
+	if !InSlice(pid) {
+		err := GetContainerInfo()
+		if err != nil {
+			return "", err
+		}
+	}
+	for _, info := range containerInfos {
+		if info.Pid == pid {
+			return info.Hostname, nil
+		}
+	}
+	return "", errors.New("pid is not the main process id")
+}
+
+func GetContainerInfo() error {
+	ctx := context.Background()
+	cli, err := client.NewClientWithOpts(client.WithVersion("1.41"))
+	if err != nil {
+		panic(err)
+	}
+
+	containerList, err := cli.ContainerList(ctx, types.ContainerListOptions{All: true})
+	if err != nil {
+		panic(err)
+	}
+
+	for _, container := range containerList {
+		containerJson, err := cli.ContainerInspect(ctx, container.ID)
+		if err != nil {
+			panic(err)
+
+		}
+		PidSlice = append(PidSlice, containerJson.State.Pid)
+		containerInfo := ContainerInfo{
+			ID:       container.ID,
+			Pid:      containerJson.State.Pid,
+			Hostname: containerJson.Config.Hostname,
+		}
+		containerInfos = append(containerInfos, containerInfo)
+		//fmt.Printf("Container ID: %s, Pid: %d\n", container.ID, container.Pid)
+	}
+	return nil
 }
 
 func main() {
-    http.Handle("/metrics", promhttp.Handler())
-    go func() {
-        for {
-            cmd := exec.Command("nvidia-smi")
-            out, err := cmd.Output()
-            if err != nil {
-                fmt.Println(err)
-            }
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		for {
+			cmd := exec.Command("nvidia-smi")
+			out, err := cmd.Output()
+			if err != nil {
+				fmt.Println(err)
+			}
 
-            processes := strings.Split(string(out), "|")
-            //fmt.Println(processes)
+			processes := strings.Split(string(out), "\n")
 
-            for _, process := range processes[1:] {
-                fmt.Println(process)
-                match := regexp.MustCompile(pidRegexPattern).FindStringSubmatch(process)
-                fmt.Println(match)
-                if match != nil {
-                    pid, _ := strconv.Atoi(match[1])
-                    dockerHostnameCmd := exec.Command("docker", "ps", "--filter", fmt.Sprintf("ancestor=%d.h264_videorecorder", pid), "--format", "{{.Names}}")
-                    hostnameBytes, err := dockerHostnameCmd.Output()
+			for _, process := range processes[1:] {
+				fmt.Println(process)
+				match := regexp.MustCompile(pidRegexPattern).FindStringSubmatch(process)
+				fmt.Println(match)
+				if match != nil {
+					processReal := strings.Fields(process)
+					fmt.Println(processReal)
+					pid, _ := strconv.Atoi(processReal[4])
 
-                    if err != nil {
-                        fmt.Println(err)
-                    }
-
-                    hostname := strings.TrimSpace(string(hostnameBytes))
-                    gpuMemMatch := regexp.MustCompile(gpuMemRegexPattern).FindStringSubmatch(process)
-
-                    if gpuMemMatch != nil {
-                        used, _ := strconv.Atoi(gpuMemMatch[1])
-                        total, _ := strconv.Atoi(gpuMemMatch[2])
-                        usagePercentage := float64(used) / float64(total) * 100
-                        gpuUsage.WithLabelValues(match[1], hostname).Set(usagePercentage)
-                    }
-                }
-            }
-        }
-    }()
-    http.ListenAndServe(":8080", nil)
+					hostname, err := GetContainerHostname(pid)
+					if err != nil {
+						fmt.Println(err)
+					}
+					used, _ := strconv.Atoi(strings.TrimRight(processReal[7], "MiB"))
+					gpuUsage.WithLabelValues(processReal[4], hostname).Set(float64(used))
+				}
+			}
+			time.Sleep(time.Second * 1)
+		}
+	}()
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		return
+	}
 }
